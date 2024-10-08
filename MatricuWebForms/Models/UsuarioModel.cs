36a4c8ba5bcea4d07d7068799143dd5613a2b57e
keneth Munoz <keneth.munoz@ucrso.info>
2024-10-07 20:47:19 -0600

    se crea script  de DB con  la tabal usuarios. Se crea el modelo de usuarios

---
+++ MatricuWebForms/Models/UsuarioModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatricuWebForms.Models
+{
+    public class UsuarioModel
+    {
+        public int ID { get; set; }
+        public string NickName { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+        public string rol { get; set; }
+        public string AdicionadoPor { get; set; }
+        public DateTime FechaAdicion { get; set; }
+        public string ModificadoPor { get; set; }
+        public DateTime FechaModificacion { get; set; }
+
+        public UsuarioModel()
+        {
+            
+        }
+
+
+    }
+}
